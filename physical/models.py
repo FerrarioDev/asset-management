cf38779503485c4ae067f4d5a4aaebdbf5caa216
Geronimo Ferrario <ferrario.dev@gmail.com>
2023-10-01 16:05:43 -0300

    feat(deskto, laptop models): Models for assets created

--- physical/models.py
+++ physical/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from users import Users, Area
 
 # Create your models here.
+class Desktop(models.Model):
+    id = models.AutoField(primary_key=True)
+    model = models.CharField(max_length=30)
+    username = models.ForeignKey(Users, on_delete=models.CASCADE)
+    area = models.ForeignKey(Area, on_delete=models.CASCADE)
+    asset_number = models.IntegerField()
+    serial_number = models.CharField(max_length=255)
+    disk_serialnumber = models.CharField(max_length=255)
+    created_at = models.DateTimeField()
+
+class Laptop(models.Model):
+    id = models.AutoField(primary_key=True)
+    model = models.CharField(max_length=30)
+    username = models.ForeignKey(Users, on_delete=models.CASCADE)
+    area = models.ForeignKey(Area, on_delete=models.CASCADE)
+    asset_number = models.IntegerField()
+    serial_number = models.CharField(max_length=255)
+    disk_serialnumber = models.CharField(max_length=255)
+    created_at = models.DateTimeField()
