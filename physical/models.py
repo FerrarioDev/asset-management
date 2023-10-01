eb8fe57ec98690344183986d197065dada6f7eb9
Geronimo Ferrario <ferrario.dev@gmail.com>
2023-10-01 17:02:49 -0300

    feat(users and area forms: Forms to create users and area created

--- physical/models.py
+++ physical/models.py
@@ -1,23 +1,37 @@
 from django.db import models
-from users import Users, Area
+from users.models import User, Area
 
 # Create your models here.
 class Desktop(models.Model):
     id = models.AutoField(primary_key=True)
     model = models.CharField(max_length=30)
-    username = models.ForeignKey(Users, on_delete=models.CASCADE)
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
     area = models.ForeignKey(Area, on_delete=models.CASCADE)
     asset_number = models.IntegerField()
     serial_number = models.CharField(max_length=255)
     disk_serialnumber = models.CharField(max_length=255)
-    created_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.id
 
 class Laptop(models.Model):
     id = models.AutoField(primary_key=True)
     model = models.CharField(max_length=30)
-    username = models.ForeignKey(Users, on_delete=models.CASCADE)
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
     area = models.ForeignKey(Area, on_delete=models.CASCADE)
     asset_number = models.IntegerField()
     serial_number = models.CharField(max_length=255)
     disk_serialnumber = models.CharField(max_length=255)
-    created_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.id
+
+class Hard_Disk(models.Model):
+    serialnumber = models.AutoField(primary_key=True)
+    size_gb = models.CharField(max_length=255, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.serialnumber
--- users/models.py
+++ users/models.py
@@ -1,14 +1,19 @@
 from django.db import models
 
-# Create your models here.
 class User(models.Model):
     id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=255)
     role = models.CharField(max_length=255)
-    area = models.CharField(max_length=255)
-    created_at = models.DateTimeField()
+    area = models.ForeignKey('Area', on_delete=models.CASCADE, related_name='users_area')  # Reference to Area model
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.username
 
 class Area(models.Model):
     id = models.AutoField(primary_key=True)
     area_name = models.CharField(max_length=255)
-    area_manager = models.ForeignKey(User, on_delete=models.CASCADE)
+    area_manager = models.ForeignKey(User, on_delete=models.CASCADE, related_name='areas_managed')  # Reference to User model
+    
+    def __str__(self):
+        return self.area_name
