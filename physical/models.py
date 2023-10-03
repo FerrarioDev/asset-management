81dca12750d1b66a5ab7e109a5d0df20ae743e69
Geronimo/Denso <ferrario.dev@gmail.com>
2023-10-03 11:48:34 -0300

    fix(assets primarykey): change from autofield to charfield to set them manually

--- physical/models.py
+++ physical/models.py
@@ -4,7 +4,7 @@ from users.models import User, Area
 # Create your models here.
 
 class Desktop(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.CharField(primary_key=True, max_length=15)
     model = models.CharField(max_length=30)
     username = models.ForeignKey(User, on_delete=models.CASCADE)
     area = models.ForeignKey(Area, on_delete=models.CASCADE)
@@ -17,7 +17,7 @@ class Desktop(models.Model):
         return self.id
 
 class Laptop(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.CharField(primary_key=True, max_length=15)
     model = models.CharField(max_length=30)
     username = models.ForeignKey(User, on_delete=models.CASCADE)
     area = models.ForeignKey(Area, on_delete=models.CASCADE)
@@ -30,7 +30,7 @@ class Laptop(models.Model):
         return self.id
 
 class Hard_Disk(models.Model):
-    serialnumber = models.AutoField(primary_key=True)
+    serialnumber = models.CharField(primary_key=True,max_length=255)
     description = models.CharField(max_length=255, default="CORE I5/2,5/6GB/240GB")
     size_gb = models.CharField(max_length=255, unique=True)
     reason = models.CharField(max_length=255, default="Disuse")
