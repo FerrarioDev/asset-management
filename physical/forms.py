eb9f1ccfff3f123e47b12a1ee3d8ffba9d2ad998
Geronimo/Denso <ferrario.dev@gmail.com>
2023-10-03 10:50:15 -0300

    feat(hard_disk): model update and form creation

--- physical/forms.py
+++ physical/forms.py
@@ -1,5 +1,5 @@
 from django import forms 
-from .models import Desktop, Laptop
+from .models import Desktop, Laptop, Hard_Disk
 class DesktopForm(forms.ModelForm):
     class Meta:
         model = Desktop
@@ -9,3 +9,9 @@ class LaptopForm(forms.ModelForm):
     class Meta:
         model = Laptop
         fields = ['id', 'model', 'username', 'area', 'asset_number', 'disk_serialnumber']
+
+class DiskForm(forms.ModelForm):
+    class Meta:
+        model = Hard_Disk
+        fields = ['serialnumber', 'size_gb']
+        
--- physical/models.py
+++ physical/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from users.models import User, Area
 
 # Create your models here.
+
 class Desktop(models.Model):
     id = models.AutoField(primary_key=True)
     model = models.CharField(max_length=30)
@@ -30,7 +31,10 @@ class Laptop(models.Model):
 
 class Hard_Disk(models.Model):
     serialnumber = models.AutoField(primary_key=True)
+    description = models.CharField(max_length=255, default="CORE I5/2,5/6GB/240GB")
     size_gb = models.CharField(max_length=255, unique=True)
+    reason = models.CharField(max_length=255, default="Disuse")
+    # image = models.ImageField(upload_to='images/')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
