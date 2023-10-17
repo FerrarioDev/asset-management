5150b4a24d0abfbdfe7fd83cc30d745d22aa6a08
Geronimo/Denso <ferrario.dev@gmail.com>
2023-10-17 12:46:16 -0300

    feat(dashboard template): dashboard basic template

--- Backend/physical/urls.py
+++ Backend/physical/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     # home/list of assets
     path('', views.AssetListView.as_view(), name='asset_list'),
     # upload asset
-    path('upload/', views.add_asset, name='upload'),
+    path('upload/', views.add_asset, name='add_asset'),
     path('disposal/', views.disk_disposal, name='disposal'),
     path('<str:asset_id>/', views.asset_detail, name='edit_asset'),
 ]
